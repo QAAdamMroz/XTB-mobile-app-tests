b0e067a816cffc15a4c189f0799f1f88e19366ef
Adam Mróz <qa.mroz.adam@gmail.com>
2024-06-28 22:03:33 +0200

    Start register demo account flow

--- pageobjects/mobile/xtb/start.page.ts
+++ pageobjects/mobile/xtb/start.page.ts
@@ -6,10 +6,22 @@ class StartPage {
     return $('//android.widget.Button');
   }
 
-  async firstPage(): Promise<void> {
-    await expect(this.welcomeBanner).toBeDisplayed();
-    await this.startButton.click();
+  get registerDemoAccountButton() {
+    return $('//android.widget.TextView[@resource-id="ButtonNewText" and @text="Open demo account"]');
   }
 
+  get headerDemo() {
+    return $('//android.widget.TextView[@resource-id="com.xtb.xmobile2:id/title"]');
+  }
+  get emailField() {
+    return $('//android.widget.EditText[@resource-id="com.xtb.xmobile2:id/emailEdit"]');
+  }
+
+  async firstPage(): Promise<void> {
+    await this.startButton.click();
+  }
+  async chooseDemoRegisterFlow(): Promise<void> {
+    await this.registerDemoAccountButton.click();
+  }
 }
 export default new StartPage();
--- tests/suits/xtb/main.ts
+++ tests/suits/xtb/main.ts
@@ -4,10 +4,23 @@ import StartPage from "../../../pageobjects/mobile/xtb/start.page";
 class main{
         async mainSuit(): Promise<void> 
         {
+    describe("Mobile app testing", () => {
     it("Start panel", async () => {
       await expect(StartPage.welcomeBanner).toBeDisplayed();
-        await StartPage.firstPage();
+        
       })
+      it("Move to next step", async () => {
+        await StartPage.firstPage();
+        })
+        describe("Register demo account process", () => {
+        it("Open register view", async () => {
+          await StartPage.chooseDemoRegisterFlow();
+          })
+          it("Open register view", async () => {
+            await expect(StartPage.headerDemo).toBeDisplayed();
+            })
+        })
+    })
 }
 }
 export default new main();
